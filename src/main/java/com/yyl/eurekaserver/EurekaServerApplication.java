b171292fb2833c1f7d84cd6334fc1f4485521d8e
yuanliang <ylyang@leapstack.cn>
2019-08-13 17:02:10 +0800

    注册中心搭建

---
+++ src/main/java/com/yyl/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.yyl.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author yangyuanliang
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
